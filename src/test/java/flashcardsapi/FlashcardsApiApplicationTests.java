c3bc778639a41efa76bd54a78ea98b3aae944905
Dean McNeil <deanolmcneil@gmail.com>
2023-08-18 20:35:27 -0400

    Initial push from local repository

---
+++ src/test/java/flashcardsapi/FlashcardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package flashcardsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
