fd23e53471509e0706be7fbdb8240dd4c35da07e
Dean McNeil <deanolmcneil@gmail.com>
2023-09-14 20:24:58 -0400

    Change out sequences for identities in sql script; move sql script out of test and into main; change TestingDatabaseConfig to not build and destroy testing database

--- src/test/java/flashcardsapi/daotests/TestingDatabaseConfig.java
+++ src/test/java/flashcardsapi/daotests/TestingDatabaseConfig.java
@@ -18,34 +18,52 @@ public class TestingDatabaseConfig {
 
     @Bean
     public DataSource dataSource() throws SQLException {
-        // Drop and then recreate the testing database under separate "admin" connection
-        adminDataSource = new SingleConnectionDataSource();
-        adminDataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
-        adminDataSource.setUsername("postgres");
-        adminDataSource.setPassword("postgres1");
-        adminJdbcTemplate = new JdbcTemplate(adminDataSource);
-        adminJdbcTemplate.update("DROP DATABASE IF EXISTS \"FlashcardsTesting\";");
-        adminJdbcTemplate.update("CREATE DATABASE \"FlashcardsTesting\";");
+        /*
+        The following commented out portion would be used if we wanted to create a new database every time we run tests.
+        However, testing for Flashcards API was having an issue with functions and triggers declared in the SQL script,
+        so for that project, the decision was made to build the testing database on the server and leave it open between
+        tests. All changes are rolled back after each test, but the testing database itself persists beyond testing.
+         */
+        // drop and recreate testing database under "admin" connection
+        //        adminDataSource = new SingleConnectionDataSource();
+        //        adminDataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
+        //        adminDataSource.setUsername("postgres");
+        //        adminDataSource.setPassword("postgres1");
+        //        adminJdbcTemplate = new JdbcTemplate(adminDataSource);
+        //        adminJdbcTemplate.update("DROP DATABASE IF EXISTS \"<TestingDatabaseName>\";");
+        //        adminJdbcTemplate.update("CREATE DATABASE \"<TestingDatabaseName>\";");
 
-        // Setup up the testing connection
+        // set up the testing connection
         SingleConnectionDataSource dataSource = new SingleConnectionDataSource();
         dataSource.setUrl("jdbc:postgresql://localhost:5432/FlashcardsTesting");
         dataSource.setUsername("postgres");
         dataSource.setPassword("postgres1");
-        dataSource.setAutoCommit(false); // So we can rollback after each test.
+        dataSource.setAutoCommit(false); // roll back after each test
 
-        // Refresh the testing database by running the setup script
-        ScriptUtils.executeSqlScript(dataSource.getConnection(), new ClassPathResource("flashcards.sql"));
+        // refresh testing database with setup script and add test data
+        /*
+        The following commented out line would be used if we wanted to create a new database every time we run tests.
+        However, testing for Flashcards API was having an issue with functions and triggers declared in the SQL script,
+        so for that project, the decision was made to build the testing database on the server and leave it open between
+        tests. All changes are rolled back after each test, but the testing database itself persists beyond testing.
+        */
+        //        ScriptUtils.executeSqlScript(dataSource.getConnection(), new ClassPathResource("<database-file-name>.sql"));
         ScriptUtils.executeSqlScript(dataSource.getConnection(), new ClassPathResource("test-data.sql"));
 
         return dataSource;
     }
 
-    @PreDestroy
-    public void cleanup() {
-        if (adminDataSource != null) {
-            adminJdbcTemplate.update("DROP DATABASE \"FlashcardsTesting\";");
-            adminDataSource.destroy();
-        }
-    }
+    /*
+    The following commented out portion would be used if we wanted to create a new database every time we run tests.
+    However, testing for Flashcards API was having an issue with functions and triggers declared in the SQL script,
+    so for that project, the decision was made to build the testing database on the server and leave it open between
+    tests. All changes are rolled back after each test, but the testing database itself persists beyond testing.
+     */
+    //    @PreDestroy
+    //    public void cleanup() {
+    //        if (adminDataSource != null) {
+    //            adminJdbcTemplate.update("DROP DATABASE \"<TestingDatabaseName>\";");
+    //            adminDataSource.destroy();
+    //        }
+    //    }
 }
--- src/test/resources/test-data.sql
+++ src/test/resources/test-data.sql
@@ -1,10 +1,17 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users (username, first_name, last_name, email, joined_date, is_user_active)
-VALUES ('username1', 'TestFirst1', 'TestLast1', 'testuser1@example.com', '9999/10/29 21:57:57', false);
+DELETE FROM deck_users;
+DELETE FROM practice_settings;
+DELETE FROM card;
+DELETE FROM deck;
+DELETE FROM login;
+DELETE FROM users;
 
-INSERT INTO users (username, first_name, last_name, email, joined_date)
-VALUES ('username2', 'TestFirst2', 'TestLast2', 'testuser2@example.com', '8888/01/01 01:01:01');
+INSERT INTO users (user_id, username, first_name, last_name, email, joined_date, is_user_active)
+VALUES (1001, 'username1', 'TestFirst1', 'TestLast1', 'testuser1@example.com', '9999/10/29 21:57:57', false);
+
+INSERT INTO users (user_id, username, first_name, last_name, email, joined_date)
+VALUES (1002, 'username2', 'TestFirst2', 'TestLast2', 'testuser2@example.com', '8888/01/01 01:01:01');
 
 INSERT INTO login (user_id, hashed_password)
 VALUES (1001, '111111111111111111111111111111111111111111111111111111111111');
